c0e2290ec899bd7655ff044076a66d3058156bf9
Taras Kushnir <taraskushnir.tk@gmail.com>
2023-01-23 21:17:32 +0100

    first commit

---
+++ src/com/ua/robot/homeWork3/Main.java
@@ -0,0 +1,14 @@
+package com.ua.robot.homeWork3;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 60;
+        int b = 4;
+        System.out.println("a + b = " + (a + b) + " ;");
+        System.out.println("a - b = " + (a - b) + " ;");
+        System.out.println("a * b = " + (a * b) + " ;");
+        System.out.println("a / b = " + (a / b) + " ;");
+
+
+    }
+}
