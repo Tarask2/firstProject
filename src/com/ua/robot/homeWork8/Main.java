39f48da59e56aae9db5c7800c2650aa15f986b7f
Taras Kushnir <taraskushnir.tk@gmail.com>
2023-02-06 19:23:28 +0100

    third commit

--- src/com/ua/robot/homeWork8/Main.java
+++ src/com/ua/robot/homeWork8/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = new int[10];
         randomArr(arr);
-        min(arr);
-        max(arr);
-        avarege(arr);
-        sum(arr);
+        System.out.println("Min number: " + min(arr));
+        System.out.println("Max number: " + max(arr));
+        System.out.println("Average number: " + avarege(arr));
+        System.out.println("Sum all elements:" + sum(arr));
 
     }
 
@@ -26,49 +26,50 @@ public class Main {
     }
 
     // 3.Знайти мінімальне значення
-    public static void min(int[] array) {
+    public static int min(int[] array) {
         int minNumber = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] < minNumber) {
                 minNumber = array[i];
             }
         }
-        System.out.println("Min number: " + minNumber);
+        return minNumber;
 
     }
 
     // 4.Знайти максимальне значення
-    public static void max(int[] arr) {
+    public static int max(int[] arr) {
         int maxNumber = arr[0];
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > maxNumber) {
                 maxNumber = arr[i];
             }
         }
-        System.out.println("Max number: " + maxNumber);
+        return maxNumber;
+
 
     }
 
     //5.Знайти середнє значення
-    public static void avarege(int[] arr) {
+    public static int avarege(int[] arr) {
         int average = 0;
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
             average = sum / arr.length;
         }
-        System.out.println("Average number: " + average);
+        return average;
 
     }
 
     //6.Знайти суму всіх елементів
-    public static void sum(int[] arr) {
+    public static int sum(int[] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
 
-        System.out.println("Sum all elements: " + sum);
+        return sum;
 
     }
 
