bc5442333f0a74b6e4707eb59340877fe45f7725
Taras Kushnir <taraskushnir.tk@gmail.com>
2023-02-18 19:34:02 +0100

    second commit

--- src/com/ua/robot/homeWork11/Parent.java
+++ src/com/ua/robot/homeWork11/Parent.java
@@ -2,16 +2,17 @@ package com.ua.robot.homeWork11;
 
 public class Parent {
 
+
     static {
         System.out.println("1");
     }
 
     {
-        System.out.println("1");
+        System.out.println("3");
     }
 
 
     public Parent() {
-        System.out.println("1");
+        System.out.println("4");
     }
 }
