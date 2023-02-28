ff961b0ad8b178633d25a8de2ef81796189d6308
Taras Kushnir <taraskushnir.tk@gmail.com>
2023-02-28 12:16:09 +0100

    first commit

---
+++ src/com/ua/robot/homeWork13/Human.java
@@ -0,0 +1,15 @@
+package com.ua.robot.homeWork13;
+
+
+class Human extends Pilot {
+
+    @Override
+    public void abilityFly() {
+        super.abilityFly();
+    }
+
+    @Override
+    public void drivingSkills() {
+        super.drivingSkills();
+    }
+}
