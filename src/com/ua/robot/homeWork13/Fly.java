c1f391cbdf0752f19add9ebf809b8eef3e508e9d
Taras Kushnir <taraskushnir.tk@gmail.com>
2023-03-01 11:41:20 +0100

    first commit

---
+++ src/com/ua/robot/homeWork13/Fly.java
@@ -0,0 +1,5 @@
+package com.ua.robot.homeWork13;
+
+public interface Fly {
+    void abilityFly();
+}
---
+++ src/com/ua/robot/homeWork13/Pilot.java
@@ -0,0 +1,9 @@
+package com.ua.robot.homeWork13;
+
+public class Pilot extends Driver implements Fly {
+    @Override
+    public void abilityFly() {
+        System.out.println("I can fly");
+    }
+
+}
