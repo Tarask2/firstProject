4fd1c7cba8b4289d5c581b5f580f3e175f3cc7c2
Taras Kushnir <taraskushnir.tk@gmail.com>
2023-03-14 16:04:20 +0100

    first commit

--- src/com/ua/robot/homeWork15/Main.java
+++ src/com/ua/robot/homeWork15/Main.java
@@ -1,14 +1,13 @@
 package com.ua.robot.homeWork15;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         //Task 1
-        Task_1 task1 = new Task_1();
+        ArrayAndLinked task1 = new ArrayAndLinked();
         task1.saveElementsArrayList(10);
         task1.saveElementsLinkedList(2000);
 
