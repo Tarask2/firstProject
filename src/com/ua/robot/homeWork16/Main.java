4e628f5e95337c25575f2e41b371c33831ef6535
Taras Kushnir <taraskushnir.tk@gmail.com>
2023-03-20 11:59:12 +0100

    second commit

--- src/com/ua/robot/homeWork16/Main.java
+++ src/com/ua/robot/homeWork16/Main.java
@@ -4,23 +4,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        Random random = new Random();
+        HashSet<Integer> hashSet = new HashSet<>();
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>();
         try {
             for (int i = 0; i < 1000; i++) {
-                Random random = new Random();
-                HashSet<Integer> hashSet = new HashSet<>();
-                TreeSet<Integer> treeSet = new TreeSet<>();
-                LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>();
-                int arr = random.nextInt(i,51);
                 hashSet.add(random.nextInt(i, 51));
                 treeSet.add(random.nextInt(i, 51));
                 linkedHashSet.add(random.nextInt(i, 51));
-
-
-                System.out.println(" TreeSet " + treeSet + "\t\t" + " HashSet " + hashSet + "\t\t" + " LinkedHashSet " + linkedHashSet);
             }
         } catch (Exception e) {
-            e.getMessage();
+            System.out.print(" TreeSet" + "\t\t" + treeSet + "\n" + " HashSet" + "\t\t" + hashSet + "\n" + " LinkedHashSet  " + linkedHashSet);
+
         }
     }
 }
