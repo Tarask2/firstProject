d64c5b088b3a48dbf3930f9d3f7147881d932681
Taras Kushnir <taraskushnir.tk@gmail.com>
2023-04-01 11:26:51 +0200

    first commit

---
+++ src/com/ua/robot/homeWork19/People.java
@@ -0,0 +1,20 @@
+package com.ua.robot.homeWork19;
+
+public class People {
+    String surname;
+
+    public People(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public String toString() {
+        return
+                surname;
+
+    }
+}
