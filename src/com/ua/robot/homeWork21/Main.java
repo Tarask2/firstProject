77f7e3879ad7dfe3410be3e7cb91cd3900bf375d
Taras Kushnir <taraskushnir.tk@gmail.com>
2023-04-11 23:17:54 +0200

    first commit

---
+++ src/com/ua/robot/homeWork21/Main.java
@@ -0,0 +1,8 @@
+package com.ua.robot.homeWork21;
+
+public class Main {
+    public static void main(String[] args) {
+        Square square = new Square();
+        System.out.println(square.square(-5));
+    }
+}
