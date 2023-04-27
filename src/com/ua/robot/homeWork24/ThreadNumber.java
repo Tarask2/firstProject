312f59d77c5d73006ba21e106678d70307502605
Taras Kushnir <taraskushnir.tk@gmail.com>
2023-04-27 16:18:04 +0200

    first commit

---
+++ src/com/ua/robot/homeWork24/ThreadNumber.java
@@ -0,0 +1,15 @@
+package com.ua.robot.homeWork24;
+
+public class ThreadNumber implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
